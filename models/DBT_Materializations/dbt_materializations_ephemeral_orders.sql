004a55467c525be7006fe7d019f47b06463dd213
Sawan K <sawangamgam@gmail.com>
2025-02-15 14:31:22 +0000

    deleting chapter5

--- models/DBT_Materializations/dbt_materializations_ephemeral_orders.sql
+++
@@ -1,9 +0,0 @@
-{{ config(
-    materialized='ephemeral'
-    ) 
-}}
-SELECT
-    customer_id,
-    COUNT(order_id) AS total_orders
-FROM FROM {{ source('RAW_DATA','RAW_ORDERS')}}
-GROUP BY customer_id
--- models/DBT_Materializations/dbt_materializations_vw.sql
+++
@@ -1,9 +0,0 @@
-{{config(
-  materialized='view'
-)
-}}
-
-SELECT * ,
-'raw_products_vw' as snf_created_by,
-current_timestamp as snf_created_date
- FROM {{ source('RAW_DATA','RAW_PRODUCTS')}}
