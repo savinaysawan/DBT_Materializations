56ff942e673cfc7d36809e4496e5aa10ee9393a7
Sawan K <sawangamgam@gmail.com>
2025-05-10 08:54:42 +0000

    added Washington state customer details

---
+++ models/customer_details/customer_detail_washington_tbl.sql
@@ -0,0 +1,16 @@
+{{config(
+  materialized='table',
+  alias='raw_customer_detail_washington_tbl',
+  unique_key='customerid'
+)
+}}
+
+select 
+customerid,
+customername,
+segment,
+country,
+state
+FROM
+{{ ref('customer_detail_tbl') }}
+where state='Washington'
